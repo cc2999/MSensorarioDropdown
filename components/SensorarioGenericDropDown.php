7196a58574369ce354b8d385cd3af524e2fe7cff
Simone Gentili <sensorario@gmail.com>
2012-06-21 03:36:13 +0200

    Refactored controller

--- components/SensorarioGenericDropDown.php
+++ components/SensorarioGenericDropDown.php
@@ -5,25 +5,38 @@
  * configuration.
  * 
  * SensorarioGenericDropDown::create(array(
-            'name' => 'Stati',
-            'select' => null,
-            'data' => Stato::getStati(),
-            'action' => 'regioni',
-            'fk' => 'stato_id',
-            'id' => 'regioni',
-        )) 
+  'name' => 'Stati',
+  'select' => null,
+  'data' => Stato::getStati(),
+  'action' => 'regioni',
+  'fk' => 'stato_id',
+  'id' => 'regioni',
+  ))
  */
 class SensorarioGenericDropDown
 {
     public static function create($params = array())
     {
-        return CHtml::dropDownList($params['name'], $params['select'], $params['data'], array('onchange' => '
+        if (isset($params['action'])) {
+            $params['htmlOptions'] = array('onchange' => '
             $.ajax({
                 url: "' . (Yii::app()->createUrl('MSensorarioDropdown/default/' . $params['action'])) . '/' . $params['fk'] . '/"+this.value,
                 success: function (data) {
                     $("#' . $params['id'] . '").html(data);
                 }
-            })'));
+            })');
+        }
+
+        echo CHtml::dropDownList($params['name'], $params['select'], $params['model'], $params['htmlOptions']);
+    }
+
+    public static function createDinamic($params = array())
+    {
+        if (count($params['model']) === 0) {
+            echo $params['error_message'];
+        } else {
+            SensorarioGenericDropDown::create($params);
+        }
     }
 
 }
--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -9,10 +9,10 @@ class DefaultController extends Controller
 
     public function actionStati()
     {
-        echo SensorarioGenericDropDown::create(array(
+        SensorarioGenericDropDown::create(array(
             'name' => 'Stati',
             'select' => null,
-            'data' => Stato::getStati(),
+            'model' => Stato::getStati(),
             'action' => 'regioni',
             'fk' => 'stato_id',
             'id' => 'regioni',
@@ -21,31 +21,28 @@ class DefaultController extends Controller
 
     public function actionRegioni($stato_id)
     {
-        $regioni = Regione::getRegione($stato_id);
-        if (count($regioni) === 0) {
-            echo 'Questo stato non ha regioni';
-        } else {
-            echo SensorarioGenericDropDown::create(array(
-                'name' => 'Regioni',
-                'select' => -1,
-                'data' => $regioni,
-                'action' => 'comuni',
-                'fk' => 'regione_id',
-                'id' => 'comuni',
-            ));
-        }
+        SensorarioGenericDropDown::createDinamic(array(
+            'model' => Regione::getRegione($stato_id),
+            'error_message' => 'Questo stato non ha regioni',
+            'name' => 'Regioni',
+            'select' => -1,
+            'action' => 'comuni',
+            'fk' => 'regione_id',
+            'id' => 'comuni',
+        ));
     }
 
     public function actionComuni($regione_id)
     {
-        $comuni = Comune::getComune($regione_id);
-        if (count($comuni) === 0) {
-            echo 'Questa regione non ha comuni';
-        } else {
-            echo CHtml::dropDownList('Comuni', -1, $comuni, array(
+        SensorarioGenericDropDown::createDinamic(array(
+            'model' => Comune::getComune($regione_id),
+            'error_message' => 'Questa regione non ha comuni',
+            'name' => 'Comuni',
+            'select' => -1,
+            'htmlOptions' => array(
                 'onchange' => 'console.log("Comune: " + this.value);'
-            ));
-        }
+            )
+        ));
     }
 
 }
