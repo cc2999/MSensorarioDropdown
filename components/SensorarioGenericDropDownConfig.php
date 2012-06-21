3b86dc2aa933966b7616217e69e782dd28314642
Simone Gentili <sensorario@gmail.com>
2012-06-21 03:48:01 +0200

    Refactored controller

---
+++ components/SensorarioGenericDropDownConfig.php
@@ -0,0 +1,43 @@
+<?php
+
+class SensorarioGenericDropDownConfig
+{
+    public static function CenterConfig($model)
+    {
+        return array(
+            'model' => $model,
+            'error_message' => 'Questo stato non ha regioni',
+            'name' => 'Regioni',
+            'select' => -1,
+            'action' => 'comuni',
+            'fk' => 'regione_id',
+            'id' => 'comuni',
+        );
+    }
+
+    public function RightConfig($model)
+    {
+        return array(
+            'model' => $model,
+            'error_message' => 'Questa regione non ha comuni',
+            'name' => 'Comuni',
+            'select' => -1,
+            'htmlOptions' => array(
+                'onchange' => 'console.log("Comune: " + this.value);'
+            )
+        );
+    }
+
+    public static function LeftConfig()
+    {
+        return array(
+            'name' => 'Stati',
+            'select' => null,
+            'model' => Stato::getStati(),
+            'action' => 'regioni',
+            'fk' => 'stato_id',
+            'id' => 'regioni',
+        );
+    }
+
+}
--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -9,40 +9,17 @@ class DefaultController extends Controller
 
     public function actionStati()
     {
-        SensorarioGenericDropDown::create(array(
-            'name' => 'Stati',
-            'select' => null,
-            'model' => Stato::getStati(),
-            'action' => 'regioni',
-            'fk' => 'stato_id',
-            'id' => 'regioni',
-        ));
+        SensorarioGenericDropDown::create(SensorarioGenericDropDownConfig::LeftConfig());
     }
 
     public function actionRegioni($stato_id)
     {
-        SensorarioGenericDropDown::createDinamic(array(
-            'model' => Regione::getRegione($stato_id),
-            'error_message' => 'Questo stato non ha regioni',
-            'name' => 'Regioni',
-            'select' => -1,
-            'action' => 'comuni',
-            'fk' => 'regione_id',
-            'id' => 'comuni',
-        ));
+        SensorarioGenericDropDown::createDinamic(SensorarioGenericDropDownConfig::CenterConfig(Regione::getRegione($stato_id)));
     }
 
     public function actionComuni($regione_id)
     {
-        SensorarioGenericDropDown::createDinamic(array(
-            'model' => Comune::getComune($regione_id),
-            'error_message' => 'Questa regione non ha comuni',
-            'name' => 'Comuni',
-            'select' => -1,
-            'htmlOptions' => array(
-                'onchange' => 'console.log("Comune: " + this.value);'
-            )
-        ));
+        SensorarioGenericDropDown::createDinamic(SensorarioGenericDropDownConfig::RightConfig(Comune::getComune($regione_id)));
     }
 
 }
